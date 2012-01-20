a0376d7df6051d84eafa56543d379cc420e70f61
Matías Aguirre <matiasaguirre@gmail.com>
2012-01-20 17:41:58 -0200

    Get '?next=' value before the session is trashed. Refs #221
    Conflicts:
    
    	social_auth/views.py

--- social_auth/views.py
+++ social_auth/views.py
@@ -122,12 +122,16 @@ def associate(request, backend):
 @dsa_view()
 def associate_complete(request, backend, *args, **kwargs):
     """Authentication complete process"""
-    if auth_complete(request, backend, request.user, *args, **kwargs):
-        url = NEW_ASSOCIATION_REDIRECT if NEW_ASSOCIATION_REDIRECT else \
-              request.session.pop(REDIRECT_FIELD_NAME, '') or \
-              DEFAULT_REDIRECT
-    else:
+    # pop redirect value before the session is trashed on login()
+    redirect_value = request.session.pop(REDIRECT_FIELD_NAME, '')
+    user = auth_complete(request, backend, request.user, *args, **kwargs)
+
+    if not user:
         url = LOGIN_ERROR_URL
+    elif isinstance(user, HttpResponse):
+        return user
+    else:
+        url = NEW_ASSOCIATION_REDIRECT or redirect_value or DEFAULT_REDIRECT
     return HttpResponseRedirect(url)
 
 
@@ -164,6 +168,8 @@ def auth_process(request, backend):
 
 def complete_process(request, backend, *args, **kwargs):
     """Authentication complete process"""
+    # pop redirect value before the session is trashed on login()
+    redirect_value = request.session.pop(REDIRECT_FIELD_NAME, '')
     user = auth_complete(request, backend, *args, **kwargs)
 
     if user and getattr(user, 'is_active', True):
@@ -184,10 +190,10 @@ def complete_process(request, backend, *args, **kwargs):
 
         # Remove possible redirect URL from session, if this is a new account,
         # send him to the new-users-page if defined.
-        url = NEW_USER_REDIRECT if NEW_USER_REDIRECT and \
-                                   getattr(user, 'is_new', False) else \
-              request.session.pop(REDIRECT_FIELD_NAME, '') or \
-              DEFAULT_REDIRECT
+        if NEW_USER_REDIRECT and getattr(user, 'is_new', False):
+            url = NEW_USER_REDIRECT
+        else:
+            url = redirect_value or DEFAULT_REDIRECT
     else:
         url = LOGIN_ERROR_URL
     return HttpResponseRedirect(url)
